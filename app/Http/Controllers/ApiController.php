1705e9863d14527c6cdf6e1da4275deb03c12aaf
mahmoodahmad100 <mahmood073@gmail.com>
2018-07-08 10:59:39 +0200

    almost done for swagger

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+/**
+ * Class ApiController
+ *
+ * @package App\Http\Controllers
+ *
+ * @SWG\Swagger(
+ *     basePath="/project_root_dir/public/api",
+ *     host="",
+ *     schemes={"http", "https"},
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="Test Project API",
+ *         description = "Test System API Documentation (please see the 'Auth' section first)"
+ *     )
+ * )
+ */
+class ApiController extends Controller
+{
+    
+}
