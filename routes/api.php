6a3a29a4a5d1de0090a193f36417cec1f08b4fa7
mahmoodahmad100 <mahmood073@gmail.com>
2018-07-08 12:07:37 +0200

    defining the routes

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,14 @@ use Illuminate\Http\Request;
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Authorization, Content-Type');
+
+/* AUTH */ 
+Route::group(['prefix' => 'auth'],function(){
+    Route::post('login', 'Api\AuthController@login')->name('login');
+    Route::get('logout', 'Api\AuthController@logout');
+    Route::get('refresh', 'Api\AuthController@refresh');
+    Route::get('me', 'Api\AuthController@me');
+});
+
+/* User */ 
+Route::resource('users','Api\UserController');
